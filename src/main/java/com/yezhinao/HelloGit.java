b99da3b5c43b17aeff73b37bd45f9fbefa301934
yezhinao <yezhinao@163.com>
2021-08-15 13:31:29 +0800

    commit first

---
+++ src/main/java/com/yezhinao/HelloGit.java
@@ -0,0 +1,27 @@
+package com.yezhinao;
+
+/**
+ * 包名称：com.yezhinao
+ * 类名称：HelloGit
+ * 类描述：
+ * 创建人： yezhinao
+ * 创建时间：2021/8/15 13:27
+ */
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git_IDEA");
+    }
+    
+    /**
+     * @Title: localTest
+     * @Description: 
+     * @Param: [name]
+     * @Return: java.lang.String
+     * @Author: yezhinao
+     * @Date: 2021/8/15 13:28
+     */
+    public String localTest(String name){
+        return null;
+    }
+}
